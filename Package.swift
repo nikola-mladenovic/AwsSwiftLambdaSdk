f824cb89227b50f32b4eee843c91fe4fc4698b20
Nikola Mladenovic <nikola@mladenovic.biz>
2018-10-08 01:59:47 +0300

    Swift 4.2 support

--- Package.swift
+++ Package.swift
@@ -1,12 +1,12 @@
-// swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:4.2
 
 import PackageDescription
 
 let package = Package(
     name: "AwsLambda",
     products: [.library(name: "AwsLambda", targets: ["AwsLambda"])],
-    dependencies: [.package(url: "https://github.com/nikola-mladenovic/AwsSwiftSign.git", from: "0.1.0")],
+    dependencies: [.package(url: "https://github.com/nikola-mladenovic/AwsSwiftSign.git", from: "0.2.0")],
     targets: [.target(name: "AwsLambda", dependencies: ["AwsSign"]),
-              .testTarget(name: "AwsLambdaTests",dependencies: ["AwsLambda"])]
+              .testTarget(name: "AwsLambdaTests",dependencies: ["AwsLambda"])],
+    swiftLanguageVersions: [.v4_2]
 )
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Swift library which enables you to invoke AWS Lambda programmatically. More deta
 <img src="https://travis-ci.org/nikola-mladenovic/AwsSwiftLambdaSdk.svg?branch=master">
 </a>
 <a href="https://developer.apple.com/swift/" target="_blank">
-<img src="https://img.shields.io/badge/Swift-4.1-orange.svg?style=flat" alt="Swift 4.1">
+<img src="https://img.shields.io/badge/Swift-4.2-orange.svg?style=flat" alt="Swift 4.2">
 </a>
 <a href="https://developer.apple.com/swift/" target="_blank">
 <img src="https://img.shields.io/badge/Platforms-iOS%20%7C%20macOS%20%7C%20watchOS%20%7C%20tvOS%20%7C%20Linux-4E4E4E.svg?colorA=EF5138" alt="Platforms iOS | macOS | watchOS | tvOS | Linux">
